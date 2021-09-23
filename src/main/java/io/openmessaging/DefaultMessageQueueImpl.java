fca9dcca8ae254d803292c86bb77e784610207f7
ingdex <871958943@qq.com>
2021-09-23 11:35:21 +0800

    add log

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -4,7 +4,7 @@ import java.nio.ByteBuffer;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.log4j.Logger;
-
+import org.apache.log4j.Logger;
 /**
  * 这是一个简单的基于内存的实现，以方便选手理解题意；
  * 实际提交时，请维持包名和类名不变，把方法实现修改为自己的内容；
@@ -13,7 +13,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     ConcurrentHashMap<String, Map<Integer, Long>> appendOffset = new ConcurrentHashMap<>();
     // ConcurrentHashMap<String, Map<Integer, Map<Long, ByteBuffer>>> appendData = new ConcurrentHashMap<>();
     iStorage storage = new iStorage();
-
+    Logger logger = Logger.getLogger(DefaultMessageQueueImpl.class);
     // getOrPutDefault 若指定key不存在，则插入defaultValue并返回
     private <K, V> V getOrPutDefault(Map<K, V> map, K key, V defaultValue){
         V retObj = map.get(key);
@@ -31,9 +31,9 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         long offset = topicOffset.getOrDefault(queueId, 0L);
         // 更新最大位点
         topicOffset.put(queueId, offset+1);
-        
+        logger.debug("append: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", fetchNum: ");
         storage.append(topic, queueId, offset, data);
-
+        
         // Map<Integer, Map<Long, ByteBuffer>> map1 = getOrPutDefault(appendData, topic, new HashMap<>());
         // Map<Long, ByteBuffer> map2 = getOrPutDefault(map1, queueId, new HashMap<>());
         // // 保存 data 中的数据
@@ -45,6 +45,8 @@ public class DefaultMessageQueueImpl extends MessageQueue {
 
     @Override
     public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
-        return storage.getRange(topic, queueId, offset, fetchNum);
+        Map<Integer, ByteBuffer> ret = storage.getRange(topic, queueId, offset, fetchNum);
+        logger.debug("getRange: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", fetchNum: " + String.valueOf(fetchNum) + " }\n\tret: " + ret.toString());
+        return ret;
     }
 }
