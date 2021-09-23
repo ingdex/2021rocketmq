5af02749c692aaab8f4dd096f827efe43b98263f
ingdex <871958943@qq.com>
2021-09-23 10:26:25 +0800

    debug

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -10,9 +10,10 @@ import org.apache.log4j.Logger;
  * 实际提交时，请维持包名和类名不变，把方法实现修改为自己的内容；
  */
 public class DefaultMessageQueueImpl extends MessageQueue {
-    ConcurrentHashMap<String, Topic> topicMap = new ConcurrentHashMap<>();
-    private static Logger logger = Logger.getLogger(DefaultMessageQueueImpl.class);
-    private int test = 2;
+    ConcurrentHashMap<String, Map<Integer, Long>> appendOffset = new ConcurrentHashMap<>();
+    // ConcurrentHashMap<String, Map<Integer, Map<Long, ByteBuffer>>> appendData = new ConcurrentHashMap<>();
+    Storage storage = new Storage();
+
     // getOrPutDefault 若指定key不存在，则插入defaultValue并返回
     private <K, V> V getOrPutDefault(Map<K, V> map, K key, V defaultValue){
         V retObj = map.get(key);
@@ -23,60 +24,27 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         return defaultValue;
     }
 
-
-    public static String bb_to_str(ByteBuffer buffer){
-        byte[] bytes;
-        if(buffer.hasArray()) {
-            bytes = buffer.array();
-        } else {
-            bytes = new byte[buffer.remaining()];
-            buffer.get(bytes);
-        }
-        return new String(bytes);
-    }
-
-    public static void printMap(Map<Integer, ByteBuffer> map) {
-        if (map.isEmpty()) {
-            System.out.println("{ }");
-            return;
-        }
-        System.out.println("{");
-        for (Map.Entry<Integer, ByteBuffer> entry : map.entrySet()) {
-            ByteBuffer buf = entry.getValue();
-            String str = bb_to_str(buf);
-            System.out.println("Key = " + entry.getKey() + ", Value = " + str);
-        }
-        System.out.println("}");
-    }
-    
     @Override
     public long append(String topic, int queueId, ByteBuffer data){
-        // logger.debug("append: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", data size" + String.valueOf(data.capacity()) + " }");
-        Topic topic_t = topicMap.get(topic);
-        if (topic_t == null) {
-            topicMap.put(topic, new Topic(topic));
-            topic_t = topicMap.get(topic);
-        }
-        long ret = topic_t.append(queueId, data);
-        logger.debug("append: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", data size: " + String.valueOf(data.capacity()) + " } \n\tret: offset = " + String.valueOf(ret));
-        if (test-- > 0) {
-            Map<Integer, ByteBuffer> retMap = getRange(topic, queueId, ret, 100);
-            ByteBuffer buf = retMap.get(0);
-            int same = buf.compareTo(data);
-            System.out.println("correct test: " + same);
-        }
-        return ret;
+        // 获取该 topic-queueId 下的最大位点 offset
+        Map<Integer, Long> topicOffset = getOrPutDefault(appendOffset, topic, new HashMap<>());
+        long offset = topicOffset.getOrDefault(queueId, 0L);
+        // 更新最大位点
+        topicOffset.put(queueId, offset+1);
+        
+        storage.append(topic, queueId, offset, data);
+
+        // Map<Integer, Map<Long, ByteBuffer>> map1 = getOrPutDefault(appendData, topic, new HashMap<>());
+        // Map<Long, ByteBuffer> map2 = getOrPutDefault(map1, queueId, new HashMap<>());
+        // // 保存 data 中的数据
+        // ByteBuffer buf = ByteBuffer.allocate(data.remaining());
+        // buf.put(data);
+        // map2.put(offset, buf);
+        return offset;
     }
 
     @Override
     public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
-        // logger.debug("getRange: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset" + String.valueOf(offset) + ", fetchNum" + String.valueOf(fetchNum) + " }");
-        // Map<Integer, ByteBuffer> ret = new HashMap<>();
-        
-        Topic topic_t = topicMap.get(topic);
-        
-        Map<Integer, ByteBuffer> ret = topic_t.getRange(queueId, offset, fetchNum);
-        logger.debug("getRange: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", fetchNum: " + String.valueOf(fetchNum) + " }\n\tret: " + ret.toString());
-        return ret;
+        return storage.getRange(topic, queueId, offset, fetchNum);
     }
 }
