6931cccfc0b8f71858c6b8667ea16d222d27b3dd
ingdex <871958943@qq.com>
2021-09-27 09:30:04 +0800

    no log

--- src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageQueueImpl.java
@@ -34,15 +34,7 @@ public class DefaultMessageQueueImpl extends MessageQueue {
         long offset = topicOffset.getOrDefault(queueId, 0L);
         // 更新最大位点
         topicOffset.put(queueId, offset+1);
-        if (queueId == 937 && (topic.equals("topic22"))){
-            logger.debug("append: topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", datasize: " + String.valueOf(data.remaining()));
-            logger.debug(data);
-            for(int i=0; i<data.limit()-1; i++) {
-                System.out.print(data.getChar(i));
-            }
-            System.out.print('\n');
-        } 
-        logger.debug("append: topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", datasize: " + String.valueOf(data.remaining()));
+        // logger.debug("append: topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", datasize: " + String.valueOf(data.remaining()));
         storage.append(topic, queueId, offset, data);
         
         // Map<Integer, Map<Long, ByteBuffer>> map1 = getOrPutDefault(appendData, topic, new HashMap<>());
@@ -57,18 +49,6 @@ public class DefaultMessageQueueImpl extends MessageQueue {
     @Override
     public Map<Integer, ByteBuffer> getRange(String topic, int queueId, long offset, int fetchNum) {
         Map<Integer, ByteBuffer> ret = storage.getRange(topic, queueId, offset, fetchNum);
-        
-        if (queueId == 937 && topic.equals("topic22")) {
-            logger.debug("getRange: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", fetchNum: " + String.valueOf(fetchNum) + " }");
-            for (Map.Entry<Integer, ByteBuffer> entry : ret.entrySet()) {
-                ByteBuffer buf = entry.getValue();
-                logger.debug("Key = " + entry.getKey() + ", DataSize = " + String.valueOf(buf.remaining()) + "\n" + buf);
-                for(int i=0; i<buf.limit()-1; i++) {
-                    System.out.print(buf.getChar(i));
-                }
-                System.out.print('\n');
-            }
-        }
         // logger.debug("getRange: { topic: " + String.valueOf(topic) + ", queueId: " + String.valueOf(queueId) + ", offset: " + String.valueOf(offset) + ", fetchNum: " + String.valueOf(fetchNum) + " }\n\tret: " + ret.toString());
         return ret;
     }
