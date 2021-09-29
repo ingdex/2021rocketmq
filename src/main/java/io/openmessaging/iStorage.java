deef9565d3d6a88c5622cebe930c17261fd95c9c
ingdex <871958943@qq.com>
2021-09-29 15:36:46 +0800

    interval set to 1000000 ns

--- src/main/java/io/openmessaging/iStorage.java
+++ src/main/java/io/openmessaging/iStorage.java
@@ -122,7 +122,7 @@ public class iStorage {
                 
                 request.future.complete(1);
             }
-        },0,500000,TimeUnit.NANOSECONDS);
+        },0,1000000,TimeUnit.NANOSECONDS);
     }
 
     //这个是个模拟批量查询的方法
